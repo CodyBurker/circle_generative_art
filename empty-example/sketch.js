174944da581a0415650c07bbcbd20570bb641ff3
Cody Burker <codyburker@gmail.com>
2022-03-18 15:17:59 -0400

    Refactor tiny dot generation

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -28,38 +28,35 @@ function drawEdgeCircle(mainCircleRadius){
   let circangle = random(0,360);
   let circvec = getcircle(circangle, mainCircleRadius/2);
   circvec.add(width/2,height/2);
-  circle(circvec.x, circvec.y, circradius);
+  circle(circvec.x, circvec.y, circradius,);
   // Draw line from center to edge of new circle
   // Get vector to put dots at edge of new circle
-  vec = createVector(circvec.x - width/2, circvec.y - height/2);
-  factor = 0.5;
-  closeness_to_edge = factor * circradius/2
-  vec.normalize().mult(mainCircleRadius/2 - closeness_to_edge);
-  vec.add(width/2,height/2);
-  fill(0);
-  circle(vec.x, vec.y, 8);
-  // Draw 3 dots at the edge of the circle with 50% chance
-  if (random(0,1) > 0.5){
-    drawDots(vec, circradius);
-  }
+  drawDots(circvec, circradius, mainCircleRadius);
 
 
 }
 
 // Draw 3 dots at the edge of the circle
-function drawDots(circleCenter, circleRadius){
+function drawDots(circleCenter, circleRadius,mainCircleRadius){
+  vec = createVector(circleCenter.x - width/2, circleCenter.y - height/2);
+  factor = 0.5;
+  closeness_to_edge = factor * circleRadius/2
+  vec.normalize().mult(mainCircleRadius/2 - closeness_to_edge);
+  vec.add(width/2,height/2);
+  fill(0);
+  circle(vec.x, vec.y, 8);
   // Get vector between center of new circle and vec
-  vec2 = createVector(circleCenter.x - width/2, circleCenter.y - height/2);
+  vec2 = createVector(vec.x - width/2, vec.y - height/2);
   vec3 = vec2.copy();
   // Create a vector that is perpendicular to vec2
   rotationAmount = 20;
   vec2.rotate(90-rotationAmount);
   vec2.normalize().mult(10);
-  vec2.add(circleCenter.x, circleCenter.y);
+  vec2.add(vec.x, vec.y);
   
   vec3.rotate(90+rotationAmount);
   vec3.normalize().mult(-10);
-  vec3.add(circleCenter.x, circleCenter.y);
+  vec3.add(vec.x, vec.y);
   circle(vec2.x, vec2.y, 5);
 // Draw another dot with 50% chance
   if(random(0,1) > 0.5){
