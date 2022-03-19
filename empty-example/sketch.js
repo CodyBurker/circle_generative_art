a9af6b064c45100a6723f5571af1ab4574472c97
Cody Burker <codyburker@gmail.com>
2022-03-18 22:16:53 -0400

    Changes.

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -5,89 +5,144 @@
 // Lines connecting circles
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(3300, 2550);
   angleMode(DEGREES);
   noLoop();
 }
 
 function draw() {
   background(255);
-  drawCircle(100,100, 100);
-  drawCircle(300,300, 100);
-  // Draw a grid of 9 circles
+  // drawCircle(100, 100, 100);
+  // drawCircle(300, 100, 100);
+  // drawCircle(100, 300, 100);
+  // drawCircle(300, 300, 100);
+  // Get 4 evenly spaced points around a centered circle
+  let centerX = width / 2;
+  let centerY = height / 2;
+  let mainCircleRadius = 800;
+  let n_circles = 8;
+  for(let i = 0; i < n_circles; i++) {
+    let angle = i * (360 / n_circles);
+    let x = centerX + mainCircleRadius * cos(angle);
+    let y = centerY + mainCircleRadius * sin(angle);
+    drawCircle(x, y, 400);
+  }
+
 }
 
 function drawCircle(centerX, centerY, mainCircleRadius) {
   stroke(0);
   fill(255);
+  // Thick stroke
+  strokeWeight(0);
+
+  // Set up color
+  fillColor = randomColor();
+  fill(fillColor);
+
   // Draw main circle at center
-  circle(centerX, centerX, mainCircleRadius);
+  circle(centerX, centerY, mainCircleRadius);
+
   // Draw a random circle on the outside of the main circle
-  circleRadius = random(.2,.5) * mainCircleRadius;
-  circleAngle = random(0,360);
-  drawEdgeCircle(centerX, centerY,mainCircleRadius, circleRadius, circleAngle);
-  // Draw a line between the two circles
-  // drawLines(smallCircleCenter, smallCircleRadius, mainCircleRadius);
+  circleRadius = random(.2, .5) * mainCircleRadius;
+  circleAngle = random(0, 360);
+
+  edgeCircleColor = randomColor(fillColor);
+  drawEdgeCircle(centerX, centerY, mainCircleRadius, circleRadius, circleAngle,edgeCircleColor);
+
+  // Rolldice to see if we draw another orbiting circle
+  circleRadius2 = random(.2, .5) * mainCircleRadius;
+  circleAngle2 =  circleAngle + 180 + random(-45, 45);
+  hasSecondCircle = rollDice(0.5);
+  if (hasSecondCircle) {
+    // Draw another circle on the outside of the main circle
+    circleRadius2 = random(.2, .5) * mainCircleRadius;
+    circleAngle2 =  circleAngle + 180 + random(-70, 70);
+    edgeCircleColor2 = randomColor(fillColor);
+    drawEdgeCircle(centerX, centerY, mainCircleRadius, circleRadius2, circleAngle2, edgeCircleColor2);
+  }
+
+  // Remove slice by adding white circle on top of everything
+  cutCircleAngle = random(0, 360);
+  fill(255);
+  stroke(255);
+  // let circVec = getcircle(centerX, centerY, cutCircleAngle, mainCircleRadius/2);
+  circle(cutCircleX, cutCircleY, 10);
+  // Draw a line from the edge of the small circle to the edge of the main circle
+  // Get center of small circle 1
+
+
 }
 
+function randomColor(colorToAvoid){
+  // Colors https://www.canva.com/colors/color-palettes/mermaid-lagoon/
+  let colors = ["#145DA0", "#0C2D48", "#2E8BC0", "#B1D4E0"];
+  // Remove colorToAvoid from colors
+  colors = colors.filter(color => color != colorToAvoid);
+  // fill(colors[random(colors.length)]);
+  colorPicked = random(colors);
+  return colorPicked;
+}
+
+
 // Draw circle on the edge of the main circle
-function drawEdgeCircle(centerX, centerY, mainCircleRadius, circradius, circangle){
-  let circvec = getcircle(circangle, mainCircleRadius/2);
-  circvec.add(centerX,centerY);
+function drawEdgeCircle(centerX, centerY, mainCircleRadius, circradius, circangle, fillColor) {
+  fill(fillColor);
+  let circvec = getcircle(centerX, centerY, circangle, mainCircleRadius / 2);
+  // circvec.add(centerX,centerY);
   circle(circvec.x, circvec.y, circradius,);
   // Draw line from center to edge of new circle
   // Get vector to put dots at edge of new circle
-  drawDots(centerX, centerY,circvec, circradius, mainCircleRadius);
+  // Draw dots on the outside of the circle
+  drawDots(centerX, centerY, circvec, circradius, mainCircleRadius);
 
 
 }
 
 // Draw 3 dots at the edge of the circle
-function drawDots(centerX, centerY,circleCenter, circleRadius,mainCircleRadius){
+function drawDots(centerX, centerY, circleCenter, circleRadius, mainCircleRadius) {
   vec = createVector(circleCenter.x - centerX, circleCenter.y - centerY);
   factor = 0.5;
-  closeness_to_edge = factor * circleRadius/2
-  vec.normalize().mult(mainCircleRadius/2 - closeness_to_edge);
-  vec.add(centerX,centerY);
-  fill(0);
-  circle(vec.x, vec.y, 8);
+  closeness_to_edge = factor * circleRadius / 2
+  vec.normalize().mult(mainCircleRadius / 2 - closeness_to_edge);
+  vec.add(centerX, centerY);
+  fill(255);
+  stroke(255)
+  let smallDotRadius = 15;
+  circle(vec.x, vec.y, smallDotRadius * 1.3);
   // Get vector between center of new circle and vec
   vec2 = createVector(vec.x - centerX, vec.y - centerY);
   vec3 = vec2.copy();
   // Create a vector that is perpendicular to vec2
   rotationAmount = 20;
-  vec2.rotate(90-rotationAmount);
-  vec2.normalize().mult(10);
+  vec2.rotate(90 - rotationAmount);
+  let distance = 30;
+  vec2.normalize().mult(distance);
   vec2.add(vec.x, vec.y);
-  
-  vec3.rotate(90+rotationAmount);
-  vec3.normalize().mult(-10);
+
+  vec3.rotate(90 + rotationAmount);
+  vec3.normalize().mult(-distance);
   vec3.add(vec.x, vec.y);
-  circle(vec2.x, vec2.y, 5);
-// Draw another dot with 50% chance
-  if(random(0,1) > 0.5){
-    circle(vec3.x, vec3.y, 5);
+  if (rollDice(0.5)) {
+    circle(vec2.x, vec2.y, smallDotRadius);
+  }
+  // Draw another dot with 50% chance
+  if (rollDice(0.5)) {
+    circle(vec3.x, vec3.y, smallDotRadius);
   }
 }
 
-// Draw lines between edges of different circles
-function drawLines(smallCircleCenter, smallCircleRadius,mainCircleRadius){
-  // Get slope of line between center of small circle and center of main circle
-  let vec = createVector(smallCircleCenter.x - width/2, smallCircleCenter.y - height/2);
-  let slope = vec.y/vec.x;
-  // Get intersection of line with main circle
-  let xintersect = (mainCircleRadius/2 - smallCircleRadius/2)/slope;
-  let yintersect = mainCircleRadius/2 - xintersect * slope;
-  // Get intersection of line small circle
-  let xintersect2 = (smallCircleRadius/2 - mainCircleRadius/2)/slope;
-  let yintersect2 = smallCircleRadius/2 - xintersect2 * slope;
-  // Draw line between main circle and small circle
-  line(width/2, height/2, xintersect, yintersect);
-}
 
 // Get point on circle
-function getcircle(circangle, circradius){
-  let xvec = cos(circangle) * circradius
-  let yvec = sin(circangle) * circradius
-  return createVector(xvec,yvec)
+function getcircle(xval, yval, circangle, circradius) {
+  let xvec = cos(circangle) * circradius + xval
+  let yvec = sin(circangle) * circradius + yval
+  return createVector(xvec, yvec)
+}
+
+function rollDice(prob) {
+  if (random(0, 1) < prob) {
+    return true;
+  }
+  return false;
 }
